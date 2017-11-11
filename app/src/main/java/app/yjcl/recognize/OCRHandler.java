d5ae8370ecc4f60a9bd86446bf97fc731c4861d2
Jeffrey Huang <jeff.huang62@gmail.com>
2017-11-11 18:37:43 -0500

    Isolate text elements from JSONobject

--- app/src/main/java/app/yjcl/recognize/OCRHandler.java
+++ app/src/main/java/app/yjcl/recognize/OCRHandler.java
@@ -14,6 +14,8 @@ import cz.msebera.android.httpclient.entity.StringEntity;
 
 import org.json.JSONObject;
 
+import java.util.Iterator;
+
 /**
  * Created by Jeff on 2017-11-11.
  */
@@ -44,8 +46,29 @@ public class OCRHandler {
             ByteArrayEntity entity = new ByteArrayEntity(input);
             httpClient.post(null, uriBase, entity, "application/octet-stream", new JsonHttpResponseHandler() {
                 public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                    JSONArray regions = null;
+                    try {
+                        regions = response.getJSONArray("regions");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                     String result = response.toString();
                     Log.e("REST Result:", result);
+
+                    StringBuilder s = new StringBuilder();
+                    try {
+                        for (int i = 0; i < regions.length(); i++){
+                            for (int j = 0; j < regions.getJSONObject(i).getJSONArray("lines").length(); j++){
+                                for (int k=0; k< regions.getJSONObject(i).getJSONArray("lines").getJSONObject(j).getJSONArray("words").length(); k++){
+                                    s.append(regions.getJSONObject(i).getJSONArray("lines").getJSONObject(j).getJSONArray("words").getJSONObject(k).getString("text"));
+                                }
+                            }
+                        }
+//                        String modifiedResult = response.getJSONArray("regions").getJSONObject(0).getJSONArray("lines").getJSONObject(0).getJSONArray("words").getJSONObject(0).getString("text");
+                        Log.e("Result", s.toString());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                 }
                 public void onFailure(JSONObject errorResponse, Throwable error) {
                     Log.e("ERROR", "failure in HTTP Request", error);
